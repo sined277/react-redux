903e598a2372f1c1453309f389404ad0472e0ffd
Dennis Tovkis <denchikdron228@gmail.com>
2023-10-13 18:49:37 +0300

    innitial React App

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="app">
+            <header className='app-header'>
+                <h1>Book Libary App</h1>
+            </header>
+        </div>
+    );
+}
+
+export default App;
